628c8c65fe0cb75c36ea988c48b35e6e9c11e3c9
risalahlele87-coder <risalahleles87@gmail.com>
2026-04-20 10:37:02 +0700

    Menambahkan Method print data untuk menampilkan data

--- introductionOOP.cpp
+++ introductionOOP.cpp
@@ -8,4 +8,10 @@ public :
     string nama;
     float nilai;
 
-   
+    void printData(){
+        cout << "NIM :" << nim << endl;
+        cout << "Nama :" << nama << endl;
+        cout << "Nilai :" << nilai << endl;
+    }
+}; //batas class
+
