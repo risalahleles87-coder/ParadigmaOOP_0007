13e7fe45836b37e22787af565da69393e861c3a2
risalahlele87-coder <risalahleles87@gmail.com>
2026-04-20 11:13:02 +0700

    Menambahkan Method printData dan menambahkan batas class

--- OPPDataBarang.cpp
+++ OPPDataBarang.cpp
@@ -9,4 +9,12 @@ public:
     string kategori;
     string tanggalProduksi;
 
- 
+ void printData(){
+        cout << "Nama Barang      : " << nama << endl;
+        cout << "Jumlah           : " << jumlah << endl;
+        cout << "Kategori         : " << kategori << endl;
+        cout << "Tanggal Produksi : " << tanggalProduksi << endl;
+    }
+
+}; // batas class
+
